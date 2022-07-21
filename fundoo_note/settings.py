d20b550b22f52b97cfe1f32055c07b7eaeeea35e
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-07-21 11:37:23 +0530

    [Ronit]Added : Customizing Authentication

--- fundoo_note/settings.py
+++ fundoo_note/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-b%92zz67_%ei&f$=((d3^itma*%%=yi5c)7+##lob(a0t80k#u
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,7 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'fundoo_note.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -84,7 +81,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -104,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -116,7 +111,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
@@ -126,3 +120,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'user.User'
--- user/views.py
+++ user/views.py
@@ -2,8 +2,8 @@
 import json
 import logging
 from django.http import JsonResponse
-from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
+from .models import User
 
 log = '%(lineno)d ** %(asctime)s ** %(message)s'
 logging.basicConfig(filename='user_views.log', filemode='a', format=log, level=logging.DEBUG)
@@ -17,7 +17,9 @@ def registration(request):
                                                          password=data.get("password"),
                                                          email=data.get("email"),
                                                          first_name=data.get("first_name"),
-                                                         last_name=data.get("last_name"))
+                                                         last_name=data.get("last_name"),
+                                                         phone_number=data.get("phone_number"),
+                                                         location=data.get("location"))
             return JsonResponse({"message": f"Data save successfully {user_registration.username}",
                                  "data": {"id": user_registration.id}}, status=201)
         return JsonResponse({"message": "Method not allow"}, status=400)
@@ -29,6 +31,7 @@ def registration(request):
 def login(request):
     try:
         data = json.loads(request.body)
+        print(data)
         if request.method == 'POST':
             login_user = authenticate(username=data.get("username"), password=data.get("password"))
             if login_user is not None:

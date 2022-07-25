0468c62729679444a54415e1496d963b0b49209a
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-07-25 11:59:42 +0530

    [Ronit]Added: using rest framework for APIView

--- user/views.py
+++ user/views.py
@@ -4,53 +4,56 @@ import logging
 from django.http import JsonResponse
 from django.contrib.auth import authenticate
 from .models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 log = '%(lineno)d ** %(asctime)s ** %(message)s'
 logging.basicConfig(filename='user_views.log', filemode='a', format=log, level=logging.DEBUG)
 
 
-def registration(request):
-    try:
-        data = json.loads(request.body)
-        if request.method == 'POST':
-            user_registration = User.objects.create_user(username=data.get("username"),
-                                                         password=data.get("password"),
-                                                         email=data.get("email"),
-                                                         first_name=data.get("first_name"),
-                                                         last_name=data.get("last_name"),
-                                                         phone_number=data.get("phone_number"),
-                                                         location=data.get("location"))
+class RegistrationAPIView(APIView):
+    def post(self, request):
+        try:
+            info = request.data
+            user_registration = User.objects.create_user(username=info.get("username"),
+                                                         password=info.get("password"),
+                                                         email=info.get("email"),
+                                                         first_name=info.get("first_name"),
+                                                         last_name=info.get("last_name"),
+                                                         phone_number=info.get("phone_number"),
+                                                         location=info.get("location"))
+            user_registration.save()
             return JsonResponse({"message": f"Data save successfully {user_registration.username}",
                                  "data": {"id": user_registration.id}}, status=201)
-        return JsonResponse({"message": "Method not allow"}, status=400)
-    except Exception as e:
-        logging.exception(e)
-        return JsonResponse({"message": "Unexpected error"}, status=400)
+        except Exception as e:
+            logging.exception(e)
+            return JsonResponse({"message": "Unexpected error"}, status=400)
 
 
-def login(request):
-    try:
-        data = json.loads(request.body)
-        if request.method == 'POST':
-            # login_user = authenticate(username=data.get("username"), password=data.get("password"))
-            login_user = authenticate(**data)
+class LoginAPIView(APIView):
+    def post(self, request):
+        try:
+            info = request.data
+            login_user = authenticate(username=info.get("username"), password=info.get("password"))
+            # login_user = authenticate(**body)
             if login_user is not None:
                 return JsonResponse({'message': f'User {login_user.username} is successfully login'}, status=200)
             else:
                 return JsonResponse({'message': 'Invalid username/password'}, status=200)
-        return JsonResponse({'message': 'Method not allow'}, status=400)
-    except Exception as e:
-        logging.exception(e)
-        return JsonResponse({'message': 'Unexpected error'}, status=400)
+        except Exception as e:
+            logging.exception(e)
+            return JsonResponse({'message': 'Unexpected error'}, status=400)
 
 
-def change_password(request):
-    try:
-        data = json.loads(request.body)
-        user = User.objects.get(username=data.get('username'))
-        user.set_password(data.get('new_password'))
-        user.save()
-        return JsonResponse({'message': 'Successfully change new password'})
-    except Exception as e:
-        print(e)
-        return JsonResponse({})
+class ChangePasswordAPIView(APIView):
+    def post(self, request):
+        try:
+            info = request.data
+            print(info)
+            user = User.objects.get(username=info.get('username'))
+            user.set_password(info.get('new_password'))
+            user.save()
+            return JsonResponse({'message': 'Successfully change new password'})
+        except Exception as e:
+            print(e)
+            return JsonResponse({})
